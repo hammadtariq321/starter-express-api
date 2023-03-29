d4d93ef52df24b45c525858d33815073e5ed757d
Hammad Tariq <hammadtariq321@gmail.com>
2023-03-29 15:54:42 +0500

    some random changes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Yo! man1')
 })
-app.listen(process.env.PORT || 3000)
+
+app.listen(process.env.PORT || 5000)
